e3c896ded2bbdc666100fff97bc8a7881bc78007
Andre Lavrinov <s66775@beuth-hochschule.de>
2018-04-26 19:22:05 +0200

    Update PlayerControls Script + Ordnerstruktur

---
+++ game/Glooms/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+
+        transform.Rotate(0, x, 0);
+    }
+}
