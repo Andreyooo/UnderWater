0eb70db9e7d5b66af1a97de9dd2d900974e54fc6
Andre Lavrinov <s66775@beuth-hochschule.de>
2018-05-13 20:08:32 +0200

    Sprite Major Update

---
+++ game/Glooms/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Animator anim;
+    private float pos;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        pos = transform.position.x;
+    }
+
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        transform.Translate(x, 0, 0);
+        if(x != pos)
+        {
+            pos = x;
+            anim.SetTrigger("Walk");
+        } else
+        {
+            anim.SetTrigger("Idle");
+        }
+    }
+}
