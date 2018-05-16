85b3332d33a82905cadc02ac6430a08d3efa9528
Andre Lavrinov <s66775@beuth-hochschule.de>
2018-05-16 23:18:03 +0200

    Update Waffen

--- game/Glooms/Assets/Scripts/Player.cs
+++ game/Glooms/Assets/Scripts/Player.cs
@@ -6,23 +6,45 @@ public class Player : MonoBehaviour
 {
     private Animator anim;
     private float pos;
+    private SpriteRenderer characterSR;
+
     private void Start()
     {
         anim = GetComponent<Animator>();
+        characterSR = GetComponent<SpriteRenderer>();
         pos = transform.position.x;
     }
 
     void Update()
     {
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 1.8f;
+        Moving();
+    }
+
+    //Movement+Animation
+    private void Moving()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 1f;
         transform.Translate(x, 0, 0);
-        if(x != pos)
+        if (x != pos)
         {
             pos = x;
             anim.SetTrigger("Walk");
-        } else
+        }
+        else
         {
             anim.SetTrigger("Idle");
         }
+    }
+
+    //isCharacterFlipped?
+    public bool GetXFlip()
+    {
+        return characterSR.flipX;
+    }
+
+    //flipping Character
+    public void FlipX(bool bo)
+    {
+        characterSR.flipX = bo;
     }
 }
---
+++ game/Glooms/Assets/Scripts/Weapon.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+    public Player player;
+    private SpriteRenderer weaponSR;
+    private bool directionRight = true;
+
+    private void Start()
+    {
+        weaponSR = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        Rotate();
+    }
+
+    //Weapon Rotation
+    private void Rotate()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        difference.Normalize();
+        float rotation_z = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        if ((rotation_z > 90f && rotation_z <= 180 || rotation_z < -90 && rotation_z > -180) && directionRight == true)
+        {
+            directionRight = false;
+            player.FlipX(true);
+            this.FlipY(true);
+        }
+        if (rotation_z <= 90f && rotation_z >= -90 && directionRight == false)
+        {
+            directionRight = true;
+            player.FlipX(false);
+            this.FlipY(false);
+        }
+        transform.rotation = Quaternion.Euler(0f, 0f, rotation_z);
+    }
+
+    //flipping Weapon
+    public void FlipY(bool bo)
+    {
+        weaponSR.flipY = bo;
+    }
+}
