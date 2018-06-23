8b6a3744d1ddf98719cfbca999e75d71a1dbecf6
Dennis Lavrinov <s66773@beuth-hochschule.de>
2018-06-23 18:38:10 +0200

    added main menu and pitched voices of gloomy's slightly

---
+++ game/Glooms/Assets/Scripts/BackgroundMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMenu : MonoBehaviour {
+
+	public Sprite[] frames; 
+	private float fps = 10;
+
+	private float index = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () { 
+		index = Time.time * fps; 
+		index = index % frames.Length; 
+		GetComponent<SpriteRenderer>().sprite = frames[(int)index]; 
+	}
+}
