ff92a9f0164a541e45b59592c781875eb30d59d2
Dennis Lavrinov <s66773@beuth-hochschule.de>
2018-07-05 13:32:07 +0200

    Added victory-Screen and a dynamic camera

---
+++ game/Glooms/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+
+public class CameraManager : MonoBehaviour {
+//Invoked when a button is pressed.
+
+	public GameObject cam;
+    public void SetParent(GameObject newParent)
+    {
+        //Makes the GameObject "newParent" the parent of the GameObject "player".
+        cam.transform.parent = newParent.transform;
+
+        //Display the parent's name in the console.
+        Debug.Log("Cam's Parent: " + cam.transform.parent.name);
+
+    }
+
+    public void DetachFromParent()
+    {
+        // Detaches the transform from its parent.
+        transform.parent = null;
+    }
+}
