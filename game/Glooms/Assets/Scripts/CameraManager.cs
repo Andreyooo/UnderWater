cac5b035dc2f1e65b5ba6592ce38b6dceb2210dd
Dennis Lavrinov <s66773@beuth-hochschule.de>
2018-07-10 00:45:37 +0200

    camera is now dynamic and zooming accordingly

--- game/Glooms/Assets/Scripts/CameraManager.cs
+++ game/Glooms/Assets/Scripts/CameraManager.cs
@@ -3,27 +3,41 @@ using UnityEngine;
 
 
 
-public class CameraManager : MonoBehaviour {
-//Invoked when a button is pressed.
+public class CameraManager : MonoBehaviour
+{
+    //Invoked when a button is pressed.
 
     public Projectile proj = null;
-    private void Start() {
+    private Vector3 originalCamPos;
+    private float originalZoom;
+    private void Start()
+    {
+        originalCamPos = gameObject.transform.position;
+        originalZoom = gameObject.GetComponent<Camera>().orthographicSize;
+
     }
 
-    private void Update(){
-        if(proj != null){
+    private void FixedUpdate()
+    {
+        if (proj != null)
+        {
             CameraFollow(proj);
         }
-        else{
+        else
+        {
             ResetCamera();
         }
     }
 
-    private void CameraFollow(Projectile projectile){
-        gameObject.transform.position = projectile.transform.position + new Vector3(0,0,-5);
+    private void CameraFollow(Projectile projectile)
+    {
+        gameObject.transform.position = projectile.transform.position + new Vector3(0, 0, -5);
+        gameObject.GetComponent<Camera>().orthographicSize = originalZoom - 10;
     }
 
-    private void ResetCamera(){
-        gameObject.transform.position = new Vector3(0,0,-99);
+    private void ResetCamera()
+    {
+        gameObject.transform.position = originalCamPos;
+        gameObject.GetComponent<Camera>().orthographicSize = originalZoom;
     }
 }
