1aa25ba4cf90339a316d478ad434840e3d40b650
Andre Lavrinov <s66775@beuth-hochschule.de>
2018-07-11 01:37:50 +0200

    Update: Zerstörbare Map izzda

---
+++ game/Glooms/Assets/Scripts/Map(Destructable)/DestroyableSpriteScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyableSpriteScript : MonoBehaviour {
+
+    private void Awake()
+    {
+        gameObject.GetComponent<Explodable>().explode();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //gameObject.GetComponent<Explodable>().explode();
+    }
+}
